9925650bc57d487aeac19d681f9a2d9c3d11a7ac
William Lim <manvinedi@gmail.com>
2018-06-11 09:37:16 +0800

    added swift version

---
+++ basic-swift.swift
@@ -0,0 +1,38 @@
+// # basic-swift
+// A handy reference for basic Swift language concepts.
+
+// ## Basic Types
+
+// * String
+// * Int // Whole numbers with no fractional component
+// * Double // Has a precision of at least 15 decimal digits
+// * Float // Can have a precision as little as 6 decimal digits
+// * Bool
+
+// ## Constants and Variables
+
+let exampleConstant = 0 // Constant
+var exampleVariable = 0 // Variable
+
+// ## Optionals
+
+let testString = "hello"
+let optionalResult = Int(testString)
+testString = "123"
+optionalResult = Int(testString)
+
+// ## Arrays
+
+
+
+// ## Sets
+
+// A set stores distinct values of the same type in a collection with no defined ordering. 
+
+// You can use a set instead of an array when:
+// 1. the order of items is not important
+// 2. or when you need to ensure that an item only appears once.
+
+
+
+
